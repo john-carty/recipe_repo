6556f4e687804f501682be6a2944a92cf688497f
john-carty <35169715+john-carty@users.noreply.github.com>
2020-01-22 17:55:00 -0500

    Added UserEntity and User Command closes #5 #4 #3 #2

---
+++ src/main/java/com/cartyjohn/reciperepo/model/UserEntity.java
@@ -0,0 +1,86 @@
+package com.cartyjohn.reciperepo.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name="users")
+public class UserEntity implements Serializable {
+
+    private static final long serialVersionUID = -7810166063346991004L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 25)
+    private String firstName;
+    @Column(nullable = false, length = 25)
+    private String lastName;
+
+    @Column(nullable = false, length = 100, unique=true)
+    private String email;
+
+    @Column(nullable = false)
+    private String encryptedPassword;
+    private String emailVerificationToken;
+    @Column(nullable = false)
+    private Boolean emailVerificationStatus = false;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean getEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
+}
